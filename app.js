d37846192941d144c96e7ac81b9ebcec0edbdf1b
rabina <rabinacathrine@gmail.com>
2020-11-20 19:02:47 +0530

    game done

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ function selectToss(){
 function updateName(){
     console.log("team1 name"+team1.name)
     console.log("team2 name"+team2.name)
+    document.getElementById("team1name").textContent=team1.name;
+    document.getElementById("team2name").textContent=team2.name;
 }
 
 function updateButtontext(){
@@ -66,11 +68,17 @@ function updateButtontext(){
     //     console.log("team 2 "+team2.name +"batting")
     // }
 //    console.log( `${toss===1?team1.name:team2.name}BATTING`);
-
+var button = document.getElementById("strikebutton")
+var result=document.getElementById("result")
+result.style.visibility=""
    //check if the game is over
    if(team1.runs.length==6 && team2.runs.length==6){
-        console.log("both team finisihed their chances");
-   }
+       button.remove();
+        // console.log("both team finisihed their chances");
+        console.log(team1.score===team2.score?"it is a draw":`${team1.score>team2.score?team1.name:team2.name} Wins`)
+   result.textContent=team1.score===team2.score?"it is a draw":`${team1.score>team2.score?team1.name:team2.name} Wins`
+
+    }
    else{
        //Strike over
        toss=
@@ -79,6 +87,7 @@ function updateButtontext(){
        toss;
    }
    console.log( `${toss===1?team1.name:team2.name}BATTING`)
+   document.getElementById("strikebutton").textContent= `${toss===1?team1.name:team2.name} BATTING`;
 }
 
 handlestrike=()=>{
@@ -102,8 +111,24 @@ handlestrike=()=>{
 function updateScores(){
     console.log("team1score"+team1.score)
     console.log("team2score"+team2.score)
+    document.getElementById("team1score").textContent=team1.score;
+    document.getElementById("team2score").textContent=team2.score;
+    updateRuns();
 }
 
+function updateRuns(){
+    var team1runs = document.getElementById("team1runs").children
+    var team2runs = document.getElementById("team2runs").children;
+
+   team1.runs.forEach((run,index)=>{
+       team1runs[index].textContent=run
+   }); 
+//for(int i =0;i<n;i++)
+
+   team2.runs.forEach((runs,i)=>{
+    team2runs[i].textContent=runs;
+   });
+}
 var calculatescore=(runs)=>{
     return runs.map(num=>{
         return num;
